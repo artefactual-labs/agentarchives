8156e9583bc7ec894e4dc73989028fd197b56263
Joseph Perry <berwin22@gmail.com>
2011-12-13 20:35:53 +0000

    Issue 830: 	update license statement
    Autoconverted from SVN (revision:2052)

--- agentarchives/archivematicaCreateStructuredDirectory.sh
+++ agentarchives/archivematicaCreateStructuredDirectory.sh
@@ -3,7 +3,7 @@
 # Copyright 2010-2011 Artefactual Systems Inc. <http://artefactual.com>
 #
 # Archivematica is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
+# it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
