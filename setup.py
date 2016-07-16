456c8f566d890c5f2759ad5ed3ddf5ad409f70a4
Hector Akamine <hakamine@artefactual.com>
2016-07-15 17:05:57 -0700

    Bump to v0.2.0

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name="agentarchives",
     description="Clients to retrieve, add, and modify records from archival management systems",
-    author="Misty De Meo",
-    author_email="mdemeo@artefactual.com",
+    author="Artefactual Systems",
+    author_email="info@artefactual.com",
     license="AGPL 3",
-    version="0.1",
+    version="0.2.0",
     packages=["agentarchives", "agentarchives.archivesspace", "agentarchives.archivists_toolkit", "agentarchives.atom"],
     install_requires=["requests>=2,<3", "mysqlclient>=1.3,<2"],
 )
