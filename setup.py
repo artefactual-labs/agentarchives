9436063b478ea044c15ba223d3fa8b7e38c008e4
Holly Becker <hbecker@artefactual.com>
2016-10-13 14:25:25 -0700

    Update package metadata

--- setup.py
+++ setup.py
@@ -3,10 +3,19 @@ from setuptools import setup
 setup(
     name="agentarchives",
     description="Clients to retrieve, add, and modify records from archival management systems",
+    url='https://github.com/artefactual-labs/agentarchives',
     author="Artefactual Systems",
     author_email="info@artefactual.com",
     license="AGPL 3",
     version="0.2.2",
     packages=["agentarchives", "agentarchives.archivesspace", "agentarchives.archivists_toolkit", "agentarchives.atom"],
     install_requires=["requests>=2,<3", "mysqlclient>=1.3,<2"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
 )
