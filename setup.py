23ff72e5e2dadb01cb1f84ca0b3ac0debd83cde6
Hector Akamine <hakamine@artefactual.com>
2017-06-05 14:31:21 -0700

    Bump version to 0.4.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author="Artefactual Systems",
     author_email="info@artefactual.com",
     license="AGPL 3",
-    version="0.3.0",
+    version="0.4.0",
     packages=["agentarchives", "agentarchives.archivesspace", "agentarchives.archivists_toolkit", "agentarchives.atom"],
     install_requires=["requests>=2,<3", "mysqlclient>=1.3,<2"],
     classifiers=[
